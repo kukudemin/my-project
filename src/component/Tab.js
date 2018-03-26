3c77120df38b118b1e3e457902b56144094e7bcc
kukudemin <718105242@qq.com>
2018-03-26 16:13:56 +0800

    Tab组件

--- src/component/Tab.js
+++ src/component/Tab.js
@@ -1,10 +1,41 @@
 import React from "react"
-export default class Tab extends React.Component{
+import {connect} from "react-redux"
+import {withRouter, NavLink} from 'react-router-dom';
+import "./Tab.less"
+class Tab extends React.Component{
     constructor(){
         super()
     }
     render(){
-        let b=17;
-        return <div>Tab</div>
+
+        return <footer className="footerBox">
+            <ul>
+                <li>
+                    <NavLink to="/" exact>
+                        <i className="iconfont icon-shuben"></i>
+                        学烘焙
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/circle">
+                        <i className="iconfont icon-tiantianquan"></i>
+                        烘焙圈
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/profile">
+                        <i className="iconfont icon-dianshi"></i>
+                        大学
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/profile">
+                        <i className="iconfont icon-home"></i>
+                        小窝
+                    </NavLink>
+                </li>
+            </ul>
+        </footer>;
     }
 }
+export default withRouter(connect()(Tab))
