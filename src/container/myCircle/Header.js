f410b93eb6248fd4e24b3260e84be3263c0583cd
zisubanxiayezi <1372779065@qq.com>
2018-03-26 20:38:14 +0800

    tijiao

---
+++ src/container/myCircle/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default class Header extends React.Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return (
+            <div>Header</div>
+        )
+    }
+}
