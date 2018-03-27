563eb113cd0a5ba0a5a6702c93a08b30c979b120
DELL <1681876324@qq.com>
2018-03-28 00:57:39 +0800

    第二次提交

---
+++ src/component/PersonalInformation/MyMedal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+export default class MyMedal extends React.Component{
+
+
+    render(){
+        return<div>MyMedal</div>
+        
+    }
+}
